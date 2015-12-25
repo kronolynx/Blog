5dba19afa7930b249c922ac92343e5ff009b8646
Johann Og <kronolynx@gmail.com>
2015-12-25 22:01:22 +0000

    Extract redundancy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
 
+  before_action :set_article, only: [:edit, :update, :show, :destroy]
+
+
     def index
         @articles = Article.all # we just get all the articles and saved them
     end
@@ -8,7 +11,7 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-         @article = Article.find(params[:id])
+        # @article = Article.find(params[:id])
     end
 
     def create
@@ -27,7 +30,7 @@ class ArticlesController < ApplicationController
     end
 
     def update
-       @article = Article.find(params[:id])
+      # @article = Article.find(params[:id])
       if @article.update(article_params)
         flash[:notice] = "Article was successfully updated"
         redirect_to article_path(@article)
@@ -41,13 +44,17 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-      @article = Article.find(params[:id])
+      #@article = Article.find(params[:id])
       @article.destroy
       flash[:notice] = "Article was successfully deleted"
       redirect_to articles_path
     end
     # method to permit the params for create
     private
+    #method to replace redundant variable
+    def set_article
+      @article = Article.find(params[:id])
+    end
     def article_params
         params.require(:article).permit(:title, :description)
     end
