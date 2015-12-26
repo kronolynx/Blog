d87fdecb64460cd4cf1189f1e3024f227839fe48
Johann Og <kronolynx@gmail.com>
2015-12-26 21:19:06 +0000

    Style errors and messages partials

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
         #render plain: params[:article].inspect #like print_r in php
         @article = Article.new(article_params)
         if  @article.save
-            flash[:notice] = "Article was successfully created"
+            flash[:success] = "Article was successfully created"
             # we need to redirect after creating the article
             # we need to pass the article because the path requires it
             # POST   article GET    /articles/:id(.:format)      articles#show
@@ -32,7 +32,7 @@ class ArticlesController < ApplicationController
     def update
       # @article = Article.find(params[:id])
       if @article.update(article_params)
-        flash[:notice] = "Article was successfully updated"
+        flash[:success] = "Article was successfully updated"
         redirect_to article_path(@article)
       else
         render 'edit'
@@ -46,7 +46,7 @@ class ArticlesController < ApplicationController
     def destroy
       #@article = Article.find(params[:id])
       @article.destroy
-      flash[:notice] = "Article was successfully deleted"
+      flash[:danger] = "Article was successfully deleted"
       redirect_to articles_path
     end
     # method to permit the params for create
