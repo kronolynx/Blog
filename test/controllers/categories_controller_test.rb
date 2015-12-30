5daad5f543d74705ffaed2b27fa0a90d59261089
Johann Og <kronolynx@gmail.com>
2015-12-30 23:42:00 +0000

    Add admin requirement to create categories

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -2,7 +2,9 @@ require 'test_helper'
 
 class CategoriesControllerTest < ActionController::TestCase
   def setup
-    @category = Category.create(name: "Sports")
+    @category = Category.create(name: "Programming")
+    # we need to simulate that an admin is logged in to test new
+    @user = User.create(username: "Krono", email: "krono@gmail.com", password: "password", admin: true)
   end
 
   test "should get categories index" do
@@ -11,6 +13,7 @@ class CategoriesControllerTest < ActionController::TestCase
   end
 
   test "should get new" do
+    session[:user_id] = @user.id # simulate log in the admin for the test
     get :new
     assert_response :success
   end
@@ -18,5 +21,12 @@ class CategoriesControllerTest < ActionController::TestCase
   test "should get show" do
     get(:show, {'id' => @category.id})
     assert_response :success
+  end
+
+  test "should redirect create when admin not logged in" do
+    assert_no_difference 'Category.count' do
+      post :create, category: { name: 'Programming' }
+    end
+    assert_redirected_to categories_path
   end
 end
--- test/integration/create_categories_test.rb
+++ test/integration/create_categories_test.rb
@@ -2,7 +2,13 @@ require 'test_helper'
 
 class CreateCategoriesTest < ActionDispatch::IntegrationTest
 
+  def setup
+     # we need to simulate that an admin is logged in to test new
+    @user = User.create(username: "Krono", email: "krono@gmail.com", password: "password", admin: true)
+  end
+
   test "get new category form and create category" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_difference 'Category.count', 1 do
@@ -13,6 +19,7 @@ class CreateCategoriesTest < ActionDispatch::IntegrationTest
   end
 
   test "invalid category submission results in failure" do
+    sign_in_as(@user, "password")
     get new_category_path
     assert_template 'categories/new'
     assert_no_difference 'Category.count' do
