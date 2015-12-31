9d2d2113a63c825da7ef1f53e5fe2503654e64d9
Johann Og <kronolynx@gmail.com>
2015-12-31 23:48:56 +0000

    Update views with categories

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -66,11 +66,11 @@ class ArticlesController < ApplicationController
 
     # method to permit the params for create
     def article_params
-        params.require(:article).permit(:title, :description)
+        params.require(:article).permit(:title, :description, category_ids: [])
     end
 
     def require_same_user
-      if current_user != @article.user || !current_user.admin?
+      if current_user != @article.user && !current_user.admin?
         flash[:danger] = "You can only edit or delete your own article"
         redirect_to root_path
       end
